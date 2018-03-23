32fc2663c5ac1be789266605311cb8093d4827d9
ctgrace88 <connor.grace88@gmail.com>
2018-03-22 19:56:40 -0600

    Integrate Emily's code

--- microcontroller/microcontroller.ino
+++ microcontroller/microcontroller.ino
@@ -1,20 +1,36 @@
-byte state[] = {0,0,0,0,0};
-int stateLoop = 0;
-int ledPin = 13;
-int left = 7;
-int forward = 6;
-int right = 5;
-int reverse = 4;
-//const int txPin = 9;
-//const int rxPin = 10;
+////////////////////////////////////////////////////////////////////////////
+// Purpose: Arduino code used to control the motor and servo on an rc car //
+// Authors: Connor Grace, Emily Sior                                      //
+////////////////////////////////////////////////////////////////////////////
+
+
+byte state[] = {0,0,0,0,0};   // Store inputs from GUI controller
+int stateLoop = 0;    // iterater for state array
+int AIN1 = 15;       // setup motor pins
+int AIN2 = 16;
+int motor_voltage = 0;  // intially set motor voltage to 0 to turn motors off. --we will need a way to adjust this for the speed
+Servo testServo;       // create test servo
+int servoPIN = 7;      // create pin for servo
+int degree_right = 0;
+int degree_left = 70;  // ?? probably need to adjust these angles
+int degree_straight = 35;
+//int ledPin = 13;
+//int left = 7;
+//int forward = 6;
+//int right = 5;
+//int reverse = 4;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);   // Set pin as OUTPUT
-  pinMode(left, OUTPUT);
-  pinMode(forward, OUTPUT);
-  pinMode(right, OUTPUT);
-  pinMode(reverse, OUTPUT);
-  Serial.begin(9600);            // Start serial communication at 9600 bps
+//  pinMode(ledPin, OUTPUT);   // Set pin as OUTPUT
+//  pinMode(left, OUTPUT);
+//  pinMode(forward, OUTPUT);
+//  pinMode(right, OUTPUT);
+//  pinMode(reverse, OUTPUT);
+  pinMode(ledPin, OUTPUT); 
+  pinMode(servoPIN, OUTPUT);    
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  Serial.begin(9600);    // Start serial communication at 9600 bps
 }
 
 void loop() {
@@ -25,28 +41,49 @@ void loop() {
       stateLoop++;
     }
   }
+
+  motor_voltage = state[0];   // set motor speed
+  
   if (state[1] == 1) {
-    digitalWrite(left, HIGH);    // turn the LED on
+    testServo.write(degree_left);
+    //digitalWrite(left, HIGH);    // turn the led on
   }
   else {
-    digitalWrite(left, LOW);   // otherwise turn it off
+    testServo.write(degree_straight);
+    //digitalWrite(left, LOW);   // otherwise turn it off
   }
+  
+  
   if (state[2] == 1) {
-    digitalWrite(forward, HIGH);    // turn the LED on
+    digitalWrite(AIN2, LOW);    
+    analogWrite(AIN1, motor_voltage);
+    //digitalWrite(forward, HIGH);    // turn the LED on
   }
   else {
-    digitalWrite(forward, LOW);   // otherwise turn it off
+    digitalWrite(AIN2, LOW);
+    digitalWrite(AIN1, LOW);
+    //digitalWrite(forward, LOW);   // otherwise turn it off
   }
+  
+  
   if (state[3] == 1) {
-    digitalWrite(right, HIGH);    // turn the LED on
+    testServo.write(degree_right);
+    //digitalWrite(right, HIGH);    // turn the LED on
   }
   else {
-    digitalWrite(right, LOW);   // otherwise turn it off
+    testServo.write(degree_straight);
+    //digitalWrite(right, LOW);   // otherwise turn it off
   }
+  
+  
   if (state[4] == 1) {
-    digitalWrite(reverse, HIGH);    // turn the LED on
+    digitalWrite(AIN1, LOW);    
+    analogWrite(AIN2, motor_voltage);
+    //digitalWrite(reverse, HIGH);    // turn the LED on
   }
   else {
-    digitalWrite(reverse, LOW);   // otherwise turn it off
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, LOW);
+    //digitalWrite(reverse, LOW);   // otherwise turn it off
   }
 }
